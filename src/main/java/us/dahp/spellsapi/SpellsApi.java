0539e84822b02563a8500852e270de8f9d378be6
nikmanG <ngilevskiy@gmail.com>
2020-10-03 17:04:06 -0400

    add barebones project

---
+++ src/main/java/us/dahp/spellsapi/SpellsApi.java
@@ -0,0 +1,17 @@
+package us.dahp.spellsapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpellsApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
