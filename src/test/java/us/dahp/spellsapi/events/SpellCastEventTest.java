a3f290887ec339ee8a2f4252e4eb628f31bcbe34
nikmanG <ngilevskiy@gmail.com>
2020-10-04 20:45:08 -0400

    add testing utils and event tests

---
+++ src/test/java/us/dahp/spellsapi/events/SpellCastEventTest.java
@@ -0,0 +1,39 @@
+package us.dahp.spellsapi.events;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import us.dahp.spellsapi.models.AbstractSpell;
+import us.dahp.spellsapi.models.SpellType;
+import us.dahp.spellsapi.utils.ServerMockUtils;
+import us.dahp.spellsapi.utils.SpellUtils;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author sirNikolai
+ * @since 04/10/2020
+ */
+public class SpellCastEventTest {
+
+    @Test
+    public void shouldPopulateFieldsCorrectly() {
+        // Given
+        ServerMock server = ServerMockUtils.getServerMockSafe();
+        PlayerMock player = server.addPlayer("TEST PLAYER");
+        AbstractSpell spell = SpellUtils.createBasicSpell("TEST SPELL", SpellType.SPELL);
+
+        // When
+        SpellCastEvent event = new SpellCastEvent(player, spell);
+
+        // Then
+        assertNotNull(event.getHandlers());
+        assertNotNull(SpellCastEvent.getHandlerList());
+        assertEquals(event.getPlayer().getName(), player.getName());
+        assertEquals(event.getSpell().getName(), spell.getName());
+    }
+}
